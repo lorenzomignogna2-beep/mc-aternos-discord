0bb41db26905668afce93089f2aae8ada1e92177
lorenzomignogna2-beep <lorenzo_mignogna.2@omnisansalvo.edu.it>
2026-07-25 20:15:09 +0200

    Update aternos_server_bot.py

--- aternos_server_bot.py
+++ aternos_server_bot.py
@@ -1,57 +1,53 @@
-import discord
 import os
+import discord
 from python_aternos import Client
-import time
 
-TOKEN = 'MTUzMDIyMzI0MzQyNjkyMjY2Nw.GfqsgD.85j6kd4jNFIXnipFE7LPmL0WO2I74pWbls-KXo'
+# Configurazione Token Discord
+TOKEN = 'INSERISCI_QUI_IL_TUO_TOKEN_DI_DISCORD'
+
+# Connessione ad Aternos
+atclient = Client()
+atclient.login('L0r3u20', 'Lorenzoscuolamedia')
+aternos = atclient.account
 
 client = discord.Client()
 
-atclient = Client()
-atclient.login('L0r3u20', 'Lorenzoscuolamedia')
-aternos = atclient
-
-
-atservers = aternos.servers
-
-myserv = atservers[0]
-
 @client.event
 async def on_ready():
-    print('we have logged in a {0.user}'.format(client))
-
+    print(f'Bot collegato come {client.user}')
 
 @client.event
 async def on_message(message):
-    username = str(message.author).split('#')[0]
-    user_message = str(message.content)
-    channel = str(message.channel.name)
-    print(f'{username}: {user_message} ({channel})')
-
     if message.author == client.user:
         return
 
+    user_message = str(message.content)
+
     if message.channel.name == 'bot-cmnds':
         if user_message.lower() == '?hello':
-            await message.channel.send(f'Hello {username}!')
+            await message.channel.send("Ciao! Il bot è attivo e pronto.")
             return
 
-    if message.channel.name == 'bot-cmnds':
         if user_message.lower() == '?server_start':
-          myserv.start()
-          while True:
-            ping = str(os.popen('mcstatus yourservername.aternos.me status | grep description').read())
-            if "offline" in ping:
-              time.sleep(1)
-            else:
-              break
-          await message.channel.send("server is now alive!!! you can join in 2-3 minutes by pasting ||yourservername.aternos.me:serverport|| in the server address.")
-          return
+            await message.channel.send("Sto provando ad accendere il server, attendi...")
+            try:
+                # Prende il primo server disponibile nell'account
+                servs = aternos.list_servers()
+                myserver = servs[0]
+                myserver.start()
+                await message.channel.send("Il server si sta avviando! Potrai entrare tra 2-3 minuti.")
+            except Exception as e:
+                await message.channel.send(f"Errore durante l'accensione: {str(e)}")
+            return
 
-    if message.channel.name == 'bot-cmnds':
         if user_message.lower() == '?server_stop':
-          myserv.stop()
-          await message.channel.send(f'server stopped')
-          return
+            try:
+                servs = aternos.list_servers()
+                myserver = servs[0]
+                myserver.stop()
+                await message.channel.send("Il server è stato spento.")
+            except Exception as e:
+                await message.channel.send(f"Errore durante lo spegnimento: {str(e)}")
+            return
 
 client.run(TOKEN)
